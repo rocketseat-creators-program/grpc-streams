77151603b9d77c95557a35daf36533e7a925315f
Lucas Santos <lhs.santoss@gmail.com>
2022-03-19 18:28:34 +0100

    refactor: fix names
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- server/handlers/Upload.js
+++ server/handlers/Upload.js
@@ -1,18 +1,19 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = (request, callback) => {
+module.exports = (request, response) => {
   const fileName = request.metadata.get('filename').toString()
-  const writeStream = fs.createWriteStream(path.resolve(__dirname, '../uploads', fileName), { flags: 'w' })
+  const filePath = path.resolve(__dirname, '../uploads', fileName)
+  const writeStream = fs.createWriteStream(filePath, { flags: 'w' })
   let bytesWritten = 0
+
   request.on('data', (payload) => {
     writeStream.write(payload.data)
     bytesWritten += payload.data.length
   })
 
-
   request.on('end', () => {
-    callback(null, {
+    response(null, {
       name: fileName,
       type: fileName.split('.').pop(),
       size: bytesWritten
