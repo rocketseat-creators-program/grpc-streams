f62c882a2eac15a88e1b5a7615fd8c607f12321c
Lucas Santos <lhs.santoss@gmail.com>
2022-03-19 19:40:27 +0100

    feat(client): implement upload
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- client/commands/upload.js
+++ client/commands/upload.js
@@ -1,27 +1,44 @@
-// for (const file of localFileList) {
-//   const filePath = path.resolve(uploadDir, file)
-//   const fileSize = fs.statSync(filePath).size
-//   const metadata = new grpc.Metadata()
-//   metadata.add('filename', file)
-//   metadata.add('size', fileSize)
+const path = require('path')
+const fs = require('fs')
+const grpc = require('@grpc/grpc-js')
 
-//   const fileReadStream = fs.createReadStream(filePath)
-//   const uploadStream = client.Upload(metadata, (err, response) => {
-//     if (err) throw err
-//     console.log(response)
-//   })
+const doUpload = (client, { fileName, filePath, fileSize }) => {
+  console.log(`=== Iniciando o Upload do arquivo ${fileName} com ${fileSize} bytes ===`)
+  const metadata = new grpc.Metadata()
+  metadata.add('filename', fileName)
+  metadata.add('size', fileSize)
 
-//   fileReadStream.on('data', (data) => {
-//     uploadStream.write({ data })
-//   })
+  const fileReadStream = fs.createReadStream(filePath)
+  const uploadStream = client.Upload(metadata, (err, response) => {
+    if (err) throw err
+    console.log(`Resposta do servidor:`)
+    console.table(response)
+    return
+  })
 
-//   fileReadStream.on('end', () => {
-//     console.log(`Upload finalizado ${file}: `, fileSize)
-//     uploadStream.end()
-//   })
-// }
+  fileReadStream.on('data', (data) => {
+    uploadStream.write({ data })
+  })
 
-module.exports = (client, path) => {
-  console.log(`Upload: ${client, path}`)
+  fileReadStream.on('end', () => {
+    console.log(`=== Upload finalizado ${fileName}: ${fileSize} bytes ===`)
+    uploadStream.end()
+    return
+  })
+}
 
+module.exports = (client, rawPath) => {
+  if (fs.statSync(rawPath).isDirectory()) {
+    const files = fs.readdirSync(rawPath)
+
+    files.forEach(file => {
+      const filePath = path.join(rawPath, file)
+      const fileSize = fs.statSync(filePath).size
+      console.log(filePath)
+      doUpload(client, { fileName: file, filePath, fileSize })
+    })
+    return
+  }
+
+  doUpload(client, { fileName: rawPath.split(path.sep).pop(), filePath: rawPath, fileSize: fs.statSync(rawPath).size })
 }
