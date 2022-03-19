256479e18b5b02aef8fc073aa107cb677688e76f
Lucas Santos <lhs.santoss@gmail.com>
2022-03-19 18:27:58 +0100

    feat: add download
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- server/handlers/Download.js
+++ server/handlers/Download.js
@@ -1,3 +1,21 @@
-module.exports = (request, response) => {
-  console.log({ request, response })
+const fs = require('fs')
+const path = require('path')
+
+module.exports = (call) => {
+  const filePath = path.resolve(__dirname, '../uploads', call.request.name)
+  if (!fs.existsSync(filePath)) {
+    call.emit('error', new Error('File not found'))
+    return call.end()
+  }
+
+  const fileStream = fs.createReadStream(filePath)
+  fileStream.on('data', (payload) => {
+    call.write({
+      data: Buffer.from(payload)
+    })
+  })
+
+  fileStream.on('end', () => {
+    call.end()
+  })
 }
