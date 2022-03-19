c462339a81be4b570874b8d894e7f999c129aba1
Lucas Santos <lhs.santoss@gmail.com>
2022-03-19 19:02:02 +0100

    refactor: rename and add error handling
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- client/commands/transform.js
+++ client/commands/transform.js
@@ -1,22 +1,29 @@
 const fs = require('fs')
 
 module.exports = (client, filePath) => {
-  const transformStream = client.Transform()
+  const serverStream = client.Transform()
   if (!fs.existsSync(filePath)) {
     console.error('Arquivo não encontrado')
     return process.exit(1)
   }
 
+  if (fs.statSync(filePath).isDirectory()) {
+    console.error('Não é possível transformar uma pasta, selecione um arquivo')
+    return process.exit(1)
+  }
+
   const fileReadStream = fs.createReadStream(filePath)
+  // Envio para o servidor
   fileReadStream.on('data', (payload) => {
-    transformStream.write({ data: Uint8Array.from(payload) })
+    serverStream.write({ data: Uint8Array.from(payload) })
   })
 
-  transformStream.on('data', (payload) => {
+  // Resposta
+  serverStream.on('data', (payload) => {
     process.stdout.write(payload.data.toString())
   })
 
   fileReadStream.on('end', () => {
-    transformStream.end()
+    serverStream.end()
   })
 }
